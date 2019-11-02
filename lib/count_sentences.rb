85dd66d7bebe7337cba0b46112fa2b45e07db6ee
alexvancap <alexvancap@gmail.com>
2019-11-02 14:10:08 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,35 @@ require 'pry'
 
 class String
 
+  attr_writer :string
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    sentence = 0
+    index = 0
+    array = [self]
 
+    while array[0].length > index
+      if (array[0][index] == "." && array[0][index -1 ] != ".")
+        sentence += 1
+      elsif (array[0][index] == "?" && array[0][index -1] != "?")
+        sentence += 1
+      elsif (array[0][index] == "!" && array[0][index -1] != "!")
+        sentence += 1
+      end
+      index += 1
+    end
+    sentence
   end
 end
